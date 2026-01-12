5ce017c39cd48ad034106341d516efe5ee0018fc
AsokTamang <ashoktmg2058@gmail.com>
2026-01-12 11:19:19 -0500

    using pydantic basemodal to validate the datas for reservation chain

--- inv_sys/test_inventory_management.py
+++ inv_sys/test_inventory_management.py
@@ -1,16 +1,23 @@
 import pytest
 from inventory_management import Inventory
-i=Inventory()
-def test_add_stock():
 
-    i.add_stock("AAPL", 10)  #here we are passing the item name and its quantity
-def test_remove_stock():
 
-    i.remove_stock("AAPL",25)
-    assert i.stock["AAPL"] == 5  #as there were total 20 quantity of items if we remove 10 then of course 10 quantities would be left
-def test_check_availability():
+@pytest.fixture   #here we are making this inventory funcion the fixture of our pytest
+def inventory():
+    i=Inventory()
+    return i
 
-    assert i.check_availability("AAPL", 5) == True   #As we have 5 quantity of aapl this condition will be true
+
+def test_add_stock(inventory):  #so in each test function we are using the inventory class returned from the fixture of pytest
+   inventory.add_stock("AAPL", 10)  #here we are passing the item name and its quantity
+   assert inventory.stock["AAPL"] == 10
+def test_remove_stock(inventory):
+
+    inventory.remove_stock("AAPL",25)
+    assert inventory.stock["AAPL"] == 5  #as there were total 20 quantity of items if we remove 10 then of course 10 quantities would be left
+def test_check_availability(inventory):
+
+    assert inventory.check_availability("AAPL", 5) == True   #As we have 5 quantity of aapl this condition will be true
 
 
 
--- pydant/practice.py
+++ pydant/practice.py
@@ -1,15 +1,17 @@
+import datetime
 from pydantic import BaseModel
+from typing import Optional
 
+data = {
+    "customer_name": "asok",
+    "reservation_date": "2026-08-24",
+    "number_of_guests": 8,
+}
+class Reservation(BaseModel):
+    customer_name: str
+    reservation_date: datetime.date
+    number_of_guests: int
+    special_requests: Optional[str] = None  # here we are making the special_requests var to be optinal
 
-class User(BaseModel):  #basemodal is used for assigning the type to the properties of a class
-    username: str
-    age: int
-    address: str
-
-
-asok = User(
-    username="asoktmg205@gmail.com",
-    age=24,
-    address='Kathmandu'
-)
-print(asok)
+r=Reservation(customer_name= data.get("customer_name"),reservation_date=data.get("reservation_date"),number_of_guests=data.get("number_of_guests"))
+print(r)
