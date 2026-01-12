422eafb978a4675feea5c5e9de24295ad9e45d88
AsokTamang <ashoktmg2058@gmail.com>
2026-01-11 23:20:57 -0500

    using context manager

--- bank_app/db/database_con.py
+++ bank_app/db/database_con.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
 import os
 import mysql.connector
-load_dotenv()  #configuring the dotenv
+
+load_dotenv()  # configuring the dotenv
+from contextlib import contextmanager
 
 
-
-
+@contextmanager
 def get_connection():
     db = mysql.connector.connect(
         host="localhost",
@@ -22,24 +23,28 @@ def get_connection():
     db_cursor = db.cursor(
         dictionary=True)  # here cursor is like a sql pointer which helps to run the sql query on databases and with using dictionary=True we
 
-    return db, db_cursor  #returning the connection object and cursor object
+    yield db_cursor  # yielding the cursor object which can be used in other functions at its current state
+
+    db_cursor.close()
+    db.close()
+
 
 def get_all_datas():
-    db,db_cursor = get_connection()
-    db_cursor.execute("SELECT * FROM expenses;")  # this will gives us the current use and host
-    datas = db_cursor.fetchall()  #fetching all the datas using sql query
-    for expense in datas:  # fetching the data returned after running the query
-        print(expense)
-    db_cursor.close()  # closing the cursor first
-    db.close()
+    with get_connection() as cursor:
+        cursor.execute("SELECT * FROM expenses;")  # this will gives us the current use and host
+        datas = cursor.fetchall()  # fetching all the datas using sql query
+        for expense in datas:  # fetching the data returned after running the query
+            print(expense)
 
-def get_expense_date_data(expense_date):   #retrieving the expense data based on expense date
-    db,db_cursor = get_connection()
-    db_cursor.execute("SELECT * FROM expenses WHERE expense_date = %s",[expense_date])  # this will gives us the current use and host
-    datas = db_cursor.fetchall()  #fetching all the datas using sql query
-    for expense in datas:  # fetching the data returned after running the query
-        print(expense)
-    db_cursor.close()  # closing the connection
-    db.close()
+
+def get_expense_date_data(expense_date):  # retrieving the expense data based on expense date
+    with get_connection() as cursor:
+        cursor.execute("SELECT * FROM expenses WHERE expense_date = %s",
+                       [expense_date])  # this will gives us the current use and host
+        datas = cursor.fetchall()  # fetching all the datas using sql query
+        for expense in datas:  # fetching the data returned after running the query
+            print(expense)
+
+
 if __name__ == '__main__':
     get_expense_date_data('2024-08-02')
