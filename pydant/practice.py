0857827afe31823b82756dc3dc495c2bbdac7c41
AsokTamang <ashoktmg2058@gmail.com>
2026-01-12 11:48:26 -0500

    formatted the date clearly

--- pydant/practice.py
+++ pydant/practice.py
@@ -1,17 +1,39 @@
 import datetime
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, ValidationError,AfterValidator
+from typing import Optional,Annotated,Any
 
 data = {
     "customer_name": "asok",
-    "reservation_date": "2026-08-24",
+    "reservation_date": "31 Dec 2025, Time 19:00",
     "number_of_guests": 8,
+    "special_requests": "Table near the window"
 }
+
+def is_valid(value: Any) -> datetime.datetime:
+    if type(value) == str:
+        return datetime.datetime.strptime(value, "%d %b %Y, Time %H:%M")
+
+    return value
+
+Reservation_Date = Annotated[str, AfterValidator(is_valid)]  #here we are storing the process of changing the type in this variable
+
 class Reservation(BaseModel):
     customer_name: str
-    reservation_date: datetime.date
+    reservation_date: Reservation_Date
     number_of_guests: int
     special_requests: Optional[str] = None  # here we are making the special_requests var to be optinal
 
-r=Reservation(customer_name= data.get("customer_name"),reservation_date=data.get("reservation_date"),number_of_guests=data.get("number_of_guests"))
-print(r)
+
+def create_reservation(data):
+    try:
+        r = Reservation(customer_name=data.get("customer_name"), reservation_date=data.get("reservation_date"),
+                        number_of_guests=data.get("number_of_guests"))
+        print(r)
+    except ValidationError as e:
+        print('Validation error occured',e)
+
+create_reservation(data)
+
+
+
+
