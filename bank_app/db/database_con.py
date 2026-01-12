4490afdb0a3f86c4378a53e9712968b5d0a2e0a0
AsokTamang <ashoktmg2058@gmail.com>
2026-01-11 23:01:47 -0500

    functional programming

--- bank_app/db/database_con.py
+++ bank_app/db/database_con.py
@@ -2,21 +2,32 @@ from dotenv import load_dotenv
 import os
 import mysql.connector
 load_dotenv()  #configuring the dotenv
-db=mysql.connector.connect(
-     host="localhost",
-     user=os.getenv("USERNAME"),
-     password=os.getenv("DB_PASSWORD"),
-     database="expense_manager"
-)
 
-if db.is_connected():
-    print('database connected')
-else:
-    print('database not connected')
+def get_connection():
+    db = mysql.connector.connect(
+        host="localhost",
+        user=os.getenv("USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        database="expense_manager"
+    )
 
-db_cursor=db.cursor(dictionary=True)  #here cursor is like a sql pointer which helps to run the sql query on databases and with using dictionary=True we are able to get the output in dictionary format
-db_cursor.execute("SELECT * FROM expenses;")  #this will gives us the current use and host
-datas = db_cursor.fetchall()
-for expense in datas:  #fetching the data returned after running the query
-    print(expense)
-db.close()   #closing the connection
+    if db.is_connected():
+        print('database connected')
+    else:
+        print('database not connected')
+
+    db_cursor = db.cursor(
+        dictionary=True)  # here cursor is like a sql pointer which helps to run the sql query on databases and with using dictionary=True we
+
+    return db, db_cursor
+
+def get_all_datas():
+    db,db_cursor = get_connection()
+    db_cursor.execute("SELECT * FROM expenses;")  # this will gives us the current use and host
+    datas = db_cursor.fetchall()  #fetching all the datas using sql query
+    for expense in datas:  # fetching the data returned after running the query
+        print(expense)
+    db_cursor.close()  # closing the connection
+    db.close()
+if __name__ == '__main__':
+    get_all_datas()
