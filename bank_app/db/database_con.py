3a41c6fc70bd0822086ae016484eaebde7a8f3d6
AsokTamang <ashoktmg2058@gmail.com>
2026-01-11 23:11:17 -0500

    retrieving the expense data based on expense date

--- bank_app/db/database_con.py
+++ bank_app/db/database_con.py
@@ -3,6 +3,9 @@ import os
 import mysql.connector
 load_dotenv()  #configuring the dotenv
 
+
+
+
 def get_connection():
     db = mysql.connector.connect(
         host="localhost",
@@ -19,7 +22,7 @@ def get_connection():
     db_cursor = db.cursor(
         dictionary=True)  # here cursor is like a sql pointer which helps to run the sql query on databases and with using dictionary=True we
 
-    return db, db_cursor
+    return db, db_cursor  #returning the connection object and cursor object
 
 def get_all_datas():
     db,db_cursor = get_connection()
@@ -27,7 +30,16 @@ def get_all_datas():
     datas = db_cursor.fetchall()  #fetching all the datas using sql query
     for expense in datas:  # fetching the data returned after running the query
         print(expense)
+    db_cursor.close()  # closing the cursor first
+    db.close()
+
+def get_expense_date_data(expense_date):   #retrieving the expense data based on expense date
+    db,db_cursor = get_connection()
+    db_cursor.execute("SELECT * FROM expenses WHERE expense_date = %s",[expense_date])  # this will gives us the current use and host
+    datas = db_cursor.fetchall()  #fetching all the datas using sql query
+    for expense in datas:  # fetching the data returned after running the query
+        print(expense)
     db_cursor.close()  # closing the connection
     db.close()
 if __name__ == '__main__':
-    get_all_datas()
+    get_expense_date_data('2024-08-02')
